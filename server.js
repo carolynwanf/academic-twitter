6b56201ed04a63e6122fa5890f026bbff7647bfc
Carolyn Wang <carolynwang@college.harvard.edu>
2022-05-12 20:26:48 -0400

    sans build?

--- server.js
+++ server.js
@@ -13,15 +13,13 @@ var port = process.env.PORT || 5000;
 var server = app.listen(port, function () {
   console.log("Node app is running on port", port);
 });
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname));
 // express.static(__dirname + "/build");
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "build", "index.html"))
-);
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 app.get("/defaultsite", (req, res) =>
-  res.sendFile(path.join(__dirname, "build", "index.html"))
+  res.sendFile(path.join(__dirname, "index.html"))
 );
 app.get("/:page", (req, res) => {
   console.log(req.params);
-  res.sendFile(path.join(__dirname, "build", `${req.params}/index.html`));
+  res.sendFile(path.join(__dirname, `${req.params}/index.html`));
 });
